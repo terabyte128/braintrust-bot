0a833fab4e16efc3ea58a672a44fc2c8d015ff43
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 01:40:38 -0700

    changes

--- braintrust_bot/models.py
+++ braintrust_bot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ChatMember(models.Model):
+    username = models.TextField(max_length=100, null=False, blank=False, unique=True)
--- braintrust_bot/views.py
+++ braintrust_bot/views.py
@@ -1,10 +1,12 @@
 import json
+import telegram
 
 from django.http.response import HttpResponse
-import telegram
 from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 
 # Create your views here.
+from braintrust_bot.models import ChatMember
 
 API_KEY = "167262782:AAFZohXUGULwNp_0x8Bh-s_AWkEaT0t0VLQ"
 
@@ -18,13 +20,73 @@ def set_webhook(request):
     else:
         return HttpResponse("Failed to set webhook")
 
+'''
+{
+   'update_id':130689848,
+   'message':{
+      'message_id':106,
+      'from':{
+         'first_name':'Sam',
+         'id':147524383,
+         'last_name':'Wolfson',
+         'username':'SamWolfson'
+      },
+      'text':'fff',
+      'chat':{
+         'first_name':'Sam',
+         'id':147524383,
+         'last_name':'Wolfson',
+         'username':'SamWolfson',
+         'type':'private'
+      },
+      'date':1462522610
+   }
+}
+'''
 
 @csrf_exempt
 def webhook(request):
     try:
         update = json.loads(request.body.decode('utf-8'))
-        print("json = " + str(update))
+        chat_id = update['message']['chat']['jd']
+        text = update.message.text
+
+        if text[0] == "/":
+            command(text[1:].split(" "), chat_id, update['message']['from']['username'])
+        else:
+            users = ChatMember.objects.all()
+
+            formatted_users = ["@" + user.username for user in users]
+
+            message_text = "%s called together the Brain Trust: %s" \
+                           % (update['message']['from']['first_name'], ", ".join(formatted_users))
+
+            bot.sendMessage(chat_id=chat_id, message=message_text, reply_to_message=update['message']['message_id'])
+
     except Exception as e:
         print(e)
 
-    return HttpResponse("success?")
+    return HttpResponse()
+
+
+def command(args, chat_id, sender):
+    if args[0] == "add" and args[1] != "":
+        try:
+            member = ChatMember(username=args[1])
+            member.save()
+            bot.sendMessage(chat_id=chat_id, message="@%s: %s was successfully added to the group." % (sender, args[1]))
+        except IntegrityError:
+            bot.sendMessage(chat_id=chat_id, message="@%s: %s is already in the group." % (sender, args[1]))
+
+    if args[0] == "remove" and args[1] != "":
+        try:
+            member = ChatMember.objects.get(username=args[1])
+            member.delete()
+            bot.sendMessage(chat_id=chat_id, message="@%s: %s was successfully deleted from the group."
+                                                     % (sender, args[1]))
+        except ChatMember.DoesNotExist:
+            bot.sendMessage(chat_id=chat_id, message="@%s: %s is not in the group." % (sender, args[1]))
+
+    else:
+        bot.sendMessage(chat_id=chat_id, message="@%s: Command not found." % sender)
+
