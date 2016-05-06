e228cd223153dc1e3c8e64ad1db52a2ff0887d75
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 02:56:17 -0700

    changes

--- django_braintrust_bot/settings.py
+++ django_braintrust_bot/settings.py
@@ -128,7 +128,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
-
