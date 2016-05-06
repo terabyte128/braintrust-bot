6056edbb7206682dfc95e5a2e48d0cd6dec73961
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 01:42:32 -0700

    changes

--- django_braintrust_bot/settings.py
+++ django_braintrust_bot/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
