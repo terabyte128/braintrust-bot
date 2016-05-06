a692feddc42518baad1f45cdc27d14c0376d7a38
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 11:56:54 -0700

    disallow setting webhooks

--- braintrust_bot/urls.py
+++ braintrust_bot/urls.py
@@ -4,7 +4,7 @@ __author__ = 'Sam'
 
 
 urlpatterns = [
-    url(r'^set_webhook', views.set_webhook, name="set_webhook"),
+    # url(r'^set_webhook', views.set_webhook, name="set_webhook"),
     url(r'^$', views.index, name="index"),
     url(r'^webhook', views.webhook, name="webbook"),
 ]
