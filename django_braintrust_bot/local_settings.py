3db714b985d95fe8bd365f4192b6fcc86e0ed06f
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 11:05:15 -0700

    local settings

---
+++ django_braintrust_bot/local_settings.py
@@ -0,0 +1,8 @@
+API_KEY = "167262782:AAFZohXUGULwNp_0x8Bh-s_AWkEaT0t0VLQ"
+SECRET_KEY = '*zj1038uddme#1q8w9%b5=^t0sro-ti)#3fa#_6g*09ry*w@6-'
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'braintrust-bot.herokuapp.com'
+]
