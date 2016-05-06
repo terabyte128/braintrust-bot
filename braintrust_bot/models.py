f4190c967b4782c57320f4b19424f3cdb6053bdb
Sam Wolfson <wolfson@cs.uw.edu>
2016-05-06 03:00:16 -0700

    Initial commit to GitHub

---
+++ braintrust_bot/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ChatMember(models.Model):
+    username = models.TextField(max_length=100, null=False, blank=False)
+    chat_id = models.IntegerField(null=False, blank=False)
+
+    class Meta:
+        unique_together = ('username', 'chat_id')
+
+    def __str__(self):
+        return self.username + "@" + str(self.chat_id)
