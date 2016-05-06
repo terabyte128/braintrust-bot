ddaa5a26df3b12538a0373b712891ea292257648
Samuel Wolfson <wolfson@cs.washington.edu>
2016-05-06 15:51:22 -0700

    Add a bunch of comments

--- braintrust_bot/urls.py
+++ braintrust_bot/urls.py
@@ -4,7 +4,14 @@ __author__ = 'Sam'
 
 
 urlpatterns = [
+    
+    # webhook URL doesn't really need to be set unless we're actually changing the hook 
+    # (security concern otherwise)
     # url(r'^set_webhook', views.set_webhook, name="set_webhook"),
+    
+    # index page to verify that bot works
     url(r'^$', views.index, name="index"),
+
+    # for retrieving webhook requests
     url(r'^webhook', views.webhook, name="webbook"),
 ]
--- braintrust_bot/views.py
+++ braintrust_bot/views.py
@@ -9,13 +9,16 @@ from django.views.decorators.csrf import csrf_exempt
 from braintrust_bot.models import ChatMember
 from django_braintrust_bot.settings import API_KEY
 
+# initialize the bot for all views
 bot = telegram.Bot(token=API_KEY)
 
 
+# This page doesn't do much, just verify that the bot is working as expected
 def index(request):
     return HttpResponse("BrainTrust Bot 2.0")
 
 
+# Set the webhook for the bot - should be disabled in production via urls.py
 def set_webhook(request):
     if request.GET.get('url'):
         bot.setWebhook(request.GET.get('url'))
@@ -24,42 +27,59 @@ def set_webhook(request):
         return HttpResponse("Failed to set webhook")
 
 
+# Handle all webhook requests - Telegram cannot send csrf tokens, so disable this functionality
 @csrf_exempt
 def webhook(request):
+    # only respond to request if the bot uses the correct API key (encoded in GET)
     if request.GET.get('API_KEY') == API_KEY:
         try:
+            # try to load info from JSON request body
             update = json.loads(request.body.decode('utf-8'))
             chat_id = update['message']['chat']['id']
             text = update['message']['text']
 
+            # if the message starts with a /, it's a command, so handle it
             if text[0] == "/":
                 send_command(text[1:].split(" "), chat_id, update['message']['from']['username'])
+            
+            # otherwise, just send a reply with everyone tagged from the chat group
             else:
+                # get users for group, except message sender
                 users = ChatMember.objects.filter(chat_id=chat_id).exclude(username=update['message']['from']['username'])
-
+                
+                # format as list starting with @, required for tagging usernames
                 formatted_users = ["@" + user.username for user in users]
 
+                # send message as reply with comma-separated list of tagged users
                 message_text = "%s called together the Brain Trust: %s" \
                                % (update['message']['from']['first_name'], ", ".join(formatted_users))
 
+                # go go gadget send message!
                 bot.sendMessage(chat_id=chat_id, text=message_text, reply_to_message_id=update['message']['message_id'])
 
+        # catch all - probably bad practice
         except Exception as e:
             print("error: " + str(e))
 
+        # request was OK, so return 200
         return HttpResponse(status=200)
     else:
+        # otherwise, unauthorized
         return HttpResponse(status=401)
 
 
+# function to handle all commands sent to the bot
 def send_command(args, chat_id, sender):
 
-    # there might be @BrianTrustBot afterwards
+    # there might be @BrianTrustBot afterwards, so get rid of it if it exists
     command = args[0].split("@")[0]
 
+    # add command - add a user
     if command == "add" and args[1]:
+        # split off @ at beginning if necessary
         member_str = args[1][1:] if args[1][0] == "@" else args[1]
 
+        # try and save user, unless they already exist in the db
         try:
             member = ChatMember(username=member_str, chat_id=chat_id)
             member.save()
@@ -67,9 +87,12 @@ def send_command(args, chat_id, sender):
         except IntegrityError:
             bot.sendMessage(chat_id=chat_id, text="@%s: %s is already in the group." % (sender, member_str))
 
+    # remove command - remove a user
     elif command == "remove" and args[1]:
+        # split off @ at beginning if necessary
         member_str = args[1][1:] if args[1][0] == "@" else args[1]
 
+        # try and delete user unless they don't exist
         try:
             member = ChatMember.objects.get(username=member_str)
             member.delete()
@@ -78,12 +101,14 @@ def send_command(args, chat_id, sender):
         except ChatMember.DoesNotExist:
             bot.sendMessage(chat_id=chat_id, text="@%s: %s is not in the group." % (sender, member_str))
 
+    # list command - list all users
     elif command == "members":
         users = ChatMember.objects.filter(chat_id=chat_id)
         formatted_users = [user.username for user in users]
 
         bot.sendMessage(chat_id=chat_id, text="Members in group: %s" % (", ".join(formatted_users)))
 
+    # otherwise it's not a real command :(
     else:
         bot.sendMessage(chat_id=chat_id, text="@%s: Command not found." % sender)
 
